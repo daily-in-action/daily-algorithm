36cb8538bebdbfdaa1d0aa5e660ddf55afde6ec6
Yomae <goldrunkim@naver.com>
2021-08-05 21:29:26 +0900

    feat: 백준 3573 두 수의 합

---
+++ yomae/3273.py
@@ -0,0 +1,23 @@
+# BOJ-3273 두 수의 합
+N = int(input())
+sequence = list(map(int,input().split()))
+sum = int(input())
+
+head = 0 # 수열의 첫번째 항
+tail = N-1 # 수열의 마지막 항
+result = 0
+sequence.sort() # 오름차순으로 정렬해줌
+
+while(head<tail): # 1씩 증감하므로 head와 tail이 같아지면 루프를 끝낸다.
+    if sequence[head]+sequence[tail]>sum: # 양 끝수의 합이 더 크다면 우측 끝 항을 하나 내려준다.
+        tail-=1
+    elif sequence[head]+sequence[tail]==sum: # 합이 같다면 쌍의 갯수를 +1하고 좌측 끝 항을 하나 올려준다.
+        result+=1
+        head+=1        
+    else: # 양 끝수의 합이 더 작다면 좌측 끝 항을 하나 올려준다.
+        head+=1
+
+print(result)
+    
+
+
