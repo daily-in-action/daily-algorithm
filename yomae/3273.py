ceed7e74e54561995b1d4f575149c8fe90050d8e
Yomae <goldrunkim@naver.com>
2021-08-06 00:01:40 +0900

    fix: 중복코드 제거, if-else구문 최적화

--- yomae/3273.py
+++ yomae/3273.py
@@ -8,16 +8,15 @@ tail = N-1 # 수열의 마지막 항
 result = 0
 sequence.sort() # 오름차순으로 정렬해줌
 
-while(head<tail): # 1씩 증감하므로 head와 tail이 같아지면 루프를 끝낸다.
-    if sequence[head]+sequence[tail]>sum: # 양 끝수의 합이 더 크다면 우측 끝 항을 하나 내려준다.
-        tail-=1
-    elif sequence[head]+sequence[tail]==sum: # 합이 같다면 쌍의 갯수를 +1하고 좌측 끝 항을 하나 올려준다.
-        result+=1
-        head+=1        
+while(head < tail): # 1씩 증감하므로 head와 tail이 같아지면 루프를 끝낸다.
+    sequence_sum = sequence[head]+sequence[tail]
+    
+    if sequence_sum == sum: # 합이 같다면 쌍의 갯수를 +1한다.
+        result += 1
+
+    if sequence_sum > sum: # 양 끝수의 합이 더 크다면 우측 끝 항을 하나 내려준다.
+        tail -= 1
     else: # 양 끝수의 합이 더 작다면 좌측 끝 항을 하나 올려준다.
-        head+=1
+        head += 1  
 
 print(result)
-    
-
-
