bfaf8514189693a8b902439defe145f84cedf462
Yomae <goldrunkim@naver.com>
2021-08-06 23:17:41 +0900

    fix: while문, 메서드 수정

--- yomae/5397.py
+++ yomae/5397.py
@@ -5,38 +5,31 @@ import sys
 
 N = int(input())
 
-def pwd_check(char): # >, <, -를 제외한 알파벳 or 숫자 체크
-    if char=='>' or char=='<' or char=='-':
-        return False
+def process(char, stack_result, stack_temp): # 조건 처리 함수
+    if char == '>':
+        if len(stack_temp) > 0:
+            stack_result.append(stack_temp.pop()) # > 인 경우 temp에서 하나를 뽑아서 result에 넘겨줌
+    elif char == '<':
+        if len(stack_result) > 0:
+            stack_temp.append(stack_result.pop()) # < 인 경우 result에서 하나를 뽑아서 temp로 넘겨줌
+    elif char == '-':
+        if len(stack_result) > 0:
+            stack_result.pop() # - 즉 백스페이스가 나온 경우에 result에서 하나를 지워준다.  
     else:
-        return True
+        stack_result.append(char) # 알파벳 or 숫자이면 result에 넣어줌     
 
-while(N!=0):
+    # else안에 if len(temp)>0 이나 if len(result)>0 조건들은 리스트가 비어있을 때 pop()을 하면 자꾸 에러가 나서 넣어줌.
+
+for i in range(N):
     test = sys.stdin.readline().rstrip()
     temp = []
-    result = []    
+    result = []
 
-    for i in range(len(test)):
-        char_ = test[i]
-
-        if pwd_check(char_):
-            result.append(char_) # 알파벳 or 숫자이면 result에 넣어줌           
-        else:
-            if char_ == '>':
-                if len(temp) > 0:
-                    result.append(temp.pop()) # > 인 경우 temp에서 하나를 뽑아서 result에 넘겨줌
-            elif char_ == '<':
-                if len(result) > 0:
-                    temp.append(result.pop()) # < 인 경우 result에서 하나를 뽑아서 temp로 넘겨줌
-            else:
-                if len(result) > 0:
-                    result.pop() # - 즉 백스페이스가 나온 경우에 result에서 하나를 지워준다.       
-
-        # else안에 if len(temp)>0 이나 if len(result)>0 조건들은 리스트가 비어있을 때 pop()을 하면 자꾸 에러가 나서 넣어줌.
+    for j in range(len(test)):
+        process(test[j], result, temp)
     
     if len(temp) > 0: # 루프를 다 돌고 나왔을 때 temp에 남아있는 문자가 있을 경우에 역순으로 result에 넘겨줌
-        for j in range(len(temp)):
+        for k in range(len(temp)):
             result.append(temp.pop())
 
     print(''.join(result))
-    N -= 1
